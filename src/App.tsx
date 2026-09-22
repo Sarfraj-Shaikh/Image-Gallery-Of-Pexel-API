e75978590df4e4f3e2ab1685af67354d6217de65
Sarfraj Shaikh <work.shaikhmosarfraj@gmail.com>
2026-09-22 14:49:40 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,32 @@ function App() {
 
   return (
     <>
-  
+      <section>
+
+        <div>
+          <div>
+            <i></i>
+            <input type="text" />
+            <i></i>
+          </div>
+
+          <div>
+            <button><i></i>Search</button>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <img src="" alt="" />
+            <p></p>
+            <p><i></i>Download</p>
+          </div>
+        </div>
+
+        <div>
+          copyright
+        </div>
+      </section>
     </>
   )
 }
